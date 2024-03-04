dd167d078ec6eead422e05342b35e3a7f8adebe4
alk-han <alkaya.alihan@gmail.com>
2024-03-04 23:01:26 +0300

    HelloText
    Translate and TextMeshPro

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    [SerializeField]
+    GameObject helloText;
+
+    string myText = "Hello World!";
+    float speed = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        // ...
-        // TODO: Build something awesome
+        print("Hello World!");
+        Application.targetFrameRate = 30;   // Fixed fps rate
+        helloText.GetComponent<TextMeshPro>().text = $"{myText} : {speed}";
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Inside the update method
+        Debug.DrawLine(Vector3.zero, new Vector3(1, 1, 1), Color.cyan);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 }
